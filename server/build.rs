42095fabedb8526aa8ece052ae8a402c3380ce55
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2018-07-19 16:12:21 +0000

    Generate slides from notes

--- server/build.rs
+++ server/build.rs
@@ -3,25 +3,36 @@ extern crate presentrs;
 use std::fs;
 use std::path::PathBuf;
 
-use presentrs::Notes;
+use presentrs::{Notes, Slides};
 
 fn main() {
     let static_dir = PathBuf::from("../static");
+    let slides_dir = static_dir.join("slides");
 
-    if !static_dir.is_dir() {
-        fs::create_dir(&static_dir).expect(&format!(
-            "Failed to create static dir at: {}",
-            static_dir
+    let mut notes =
+        Notes::from_markdown("../notes.md").expect("Failed to read notes");
+
+    create_dir(&static_dir);
+    create_dir(&slides_dir);
+
+    notes.animate_steps().expect("Failed to animate slide steps");
+    notes.generate_html(static_dir).expect("Failed to generate notes file");
+
+    let slides = Slides::from_notes(&notes).expect("Failed to generate slides");
+
+    slides.write_to(slides_dir).expect("Failed to write slides");
+}
+
+fn create_dir<P: AsRef<Path>>(directory: P) {
+    let directory = directory.as_ref();
+
+    if !directory.is_dir() {
+        fs::create_dir(&directory).expect(&format!(
+            "Failed to create directory at: {}",
+            directory
                 .canonicalize()
-                .unwrap_or_else(|_| static_dir.clone())
+                .unwrap_or_else(|_| directory.to_owned())
                 .display(),
         ));
     }
-
-    Notes::from_markdown("../notes.md")
-        .expect("Failed to read notes")
-        .animate_steps()
-        .expect("Failed to animate slide steps")
-        .generate_html(static_dir)
-        .expect("Failed to generate notes file");
 }
