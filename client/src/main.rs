ea8e22ca417e06582763981be8f9193a0b6e8ad8
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2021-08-28 15:08:17 +0000

    Use newer version of `presentrs`
    Replace `cargo-web` with `trunk`, and use a newer version of Yew.

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,11 +1,5 @@
-extern crate presentrs;
-extern crate yew;
-
 use presentrs::Presentrs;
-use yew::prelude::*;
 
 fn main() {
-    yew::initialize();
-    App::<Presentrs>::new().mount_to_body();
-    yew::run_loop();
+    yew::start_app::<Presentrs>();
 }
