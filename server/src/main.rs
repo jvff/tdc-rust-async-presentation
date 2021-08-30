e1d22d8f9ba0ea4e81b6fa1843ca28e374a1fc12
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2021-08-30 17:47:40 +0000

    Add some log output to the server
    Log a message when it starts listening, and trace the requests it
    handles.

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,24 +1,30 @@
 use {
     axum::{http::StatusCode, service::get, Router, Server},
     std::{convert::Infallible, io, net::SocketAddr},
-    tower_http::services::ServeDir,
+    tower_http::{services::ServeDir, trace::TraceLayer},
+    tracing::info,
 };
 
 #[tokio::main]
 async fn main() -> Result<(), hyper::Error> {
+    tracing_subscriber::fmt::init();
+
     let address = SocketAddr::from(([0, 0, 0, 0], 8182));
 
-    let app = Router::new().nest(
-        "/",
-        get(ServeDir::new("../dist")).handle_error(
-            |error: io::Error| -> Result<_, Infallible> {
-                Ok((
-                    StatusCode::INTERNAL_SERVER_ERROR,
-                    format!("Failed to serve file: {}", error),
-                ))
-            },
-        ),
-    );
+    let app = Router::new()
+        .nest(
+            "/",
+            get(ServeDir::new("../dist")).handle_error(
+                |error: io::Error| -> Result<_, Infallible> {
+                    Ok((
+                        StatusCode::INTERNAL_SERVER_ERROR,
+                        format!("Failed to serve file: {}", error),
+                    ))
+                },
+            ),
+        )
+        .layer(TraceLayer::new_for_http());
 
+    info!("Listening on {}", address);
     Server::bind(&address).serve(app.into_make_service()).await
 }
