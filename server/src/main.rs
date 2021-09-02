2f232e4fbaa7a7ffa9838bdd5b637d9262c74c53
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2021-09-02 13:52:33 +0000

    Use the server provided by `presentrs`
    Which now supports WebSockets to synchronize slide positions.

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,8 +1,6 @@
 use {
-    axum::{http::StatusCode, service::get, Router, Server},
-    std::{convert::Infallible, io, net::SocketAddr},
-    tower_http::{services::ServeDir, trace::TraceLayer},
-    tracing::info,
+    axum::Server, presentrs::Presentrs, std::net::SocketAddr,
+    tower_http::trace::TraceLayer, tracing::info,
 };
 
 #[tokio::main]
@@ -11,19 +9,7 @@ async fn main() -> Result<(), hyper::Error> {
 
     let address = SocketAddr::from(([0, 0, 0, 0], 8182));
 
-    let app = Router::new()
-        .nest(
-            "/",
-            get(ServeDir::new("../dist")).handle_error(
-                |error: io::Error| -> Result<_, Infallible> {
-                    Ok((
-                        StatusCode::INTERNAL_SERVER_ERROR,
-                        format!("Failed to serve file: {}", error),
-                    ))
-                },
-            ),
-        )
-        .layer(TraceLayer::new_for_http());
+    let app = Presentrs::new("dist").layer(TraceLayer::new_for_http());
 
     info!("Listening on {}", address);
     Server::bind(&address).serve(app.into_make_service()).await
